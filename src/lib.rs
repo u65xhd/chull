58a5baf0ec61a836b0e6f79848254a0921056b68
daidanretsu <daidanretsu@outlook.jp>
2020-08-18 21:08:40 +0900

    fix lib comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 //! Convex hull approximation for rust based on [Quick hull](http://citeseerx.ist.psu.edu/viewdoc/summary;jsessionid=C57E2269B0D64504B97E8469F6A1315D?doi=10.1.1.117.405).
+//! Available in 3-D or 2-D for now.
 //! ## Examples
 //! ```
 //! use chull::ConvexHull;
