90a63cfe6694cc18cb0c792d113cf425d2ca7228
daidanretsu <daidanretsu@outlook.jp>
2020-08-28 18:01:17 +0900

    ver up

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,10 @@
 //! assert_eq!(i.len(), 6 * 2 * 3);
 //! ```
 //! ## If the results are inaccurate
-//! If the calculation results are inaccurate due to rounding errors, an error may occur. In such cases, the use of integer types such as [```i128```](https://doc.rust-lang.org/std/primitive.i128.html) or [```BigDecimal```](https://docs.rs/bigdecimal/) may improve the result.
+//! If the calculation results are inaccurate due to rounding errors, an error may occur. In such cases, the use of integer types such as [```BigInt```](https://docs.rs/num-bigint/) may improve the result.
 //! ```
 //! use chull::ConvexHull;
+//! use num_bigint::{BigInt, ToBigInt};
 //!
 //! let p1 = vec![1.0, 0.0, 0.0];
 //! let p2 = vec![0.0, 0.001, 0.0];
@@ -33,11 +34,12 @@
 //! let p5 = vec![0.0, -0.001, 0.0];
 //! let p6 = vec![0.0, 0.0, -0.00001];
 //! let points_float = vec![p1, p2, p3, p4, p5, p6];
-//! let mut points_i128 = Vec::new();
+//! let mut points_int = Vec::new();
 //! for point_float in &points_float{
-//!     points_i128.push(point_float.iter().map(|x| (x*1_000_000.0) as i128).collect::<Vec<_>>());
+//!     points_int.push(point_float.iter().map(|x| (x*1_000_000.0).to_bigint().unwrap()).collect::<Vec<_>>());
 //! }
-//! let octahedron = ConvexHull::try_new(&points_i128, 10, None).unwrap();
+//! // Since there is no rounding error when using BigInt, the threshold value can be zero.
+//! let octahedron = ConvexHull::try_new(&points_int, 0, None).unwrap();
 //! // The following are likely to be errors
 //! //let octahedron = ConvexHull::try_new(&points_float, std::f32::EPSILON, None).unwrap();
 //! ```
